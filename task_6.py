06ba1c9a3d0466e243fe08fc79ad0ba0b575a66a
Денис С. Лабунский <labunskii_ds@edu.kvantorium-pk.ru>
2020-02-10 16:32:36 +0900

    done

--- task_6.py
+++ task_6.py
@@ -5,6 +5,13 @@ from pyrob.api import *
 
 @task
 def task_5_3():
+    while not wall_is_beneath():
+            move_right()
+    if wall_is_beneath():
+        while True:
+            move_right()
+            if not wall_is_beneath()==True:
+                break
     pass
 
 
--- task_7.py
+++ task_7.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 
 from pyrob.api import *
-
-
 @task
 def task_5_4():
+    while not wall_is_beneath():
+        move_down()
+    if wall_is_beneath():
+        while True:
+            move_right()
+            if not wall_is_beneath():
+                move_down(1)
+                move_left(1)
+                if not wall_is_on_the_left()==True:
+                     break
+    if not wall_is_on_the_left():
+            while True:
+                move_left()
+                if wall_is_on_the_left()==True:
+                     break-
     pass
 
 
